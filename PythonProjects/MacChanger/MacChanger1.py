ec900ef578352038be91d451813348f710309884
joltrome <93308954+joltrome@users.noreply.github.com>
2025-09-25 22:40:44 +0900

    Update MacChanger1.py

--- PythonProjects/MacChanger/MacChanger1.py
+++ PythonProjects/MacChanger/MacChanger1.py
@@ -3,22 +3,27 @@
 import subprocess
 import optparse
 
+def change_mac(interface,new_mac_address):
+    print("[+] Changing MAC Address for " + interface + " to " + new_mac_address)
+
+    # subprocess.call("ifconfig " +  interface + " down", shell=True)
+    # subprocess.call("ifconfig " +  interface + " hw ether " +  new_mac , shell=True)
+    # subprocess.call("ifconfig " +  interface + " up", shell=True)
+    # subprocess.call("ifconfig", shell=True)
+
+    subprocess.call(["ifconfig", interface, "down"])
+    subprocess.call(["ifconfig", interface, "hw", "ether", new_mac_address])
+    subprocess.call(["ifconfig", interface, "up"])
+    subprocess.call(["ifconfig"])
+
+
 parser = optparse.OptionParser()
 parser.add_option("-i", "--interface", dest="interface", help="Interface to change mac address")
 parser.add_option("-a", "--address", dest="address", help="New mac address")
 (options, arguments) = parser.parse_args()
 
-interface = options.interface
-new_mac= options.address
+# interface = options.interface
+# new_mac_address= options.address
 
-print("[+] Changing MAC Address for " + interface + " to " + new_mac)
+change_mac(options.interface,options.address)
 
-# subprocess.call("ifconfig " +  interface + " down", shell=True)
-# subprocess.call("ifconfig " +  interface + " hw ether " +  new_mac , shell=True)
-# subprocess.call("ifconfig " +  interface + " up", shell=True)
-# subprocess.call("ifconfig", shell=True)
-
-subprocess.call(["ifconfig", interface, "down"])
-subprocess.call(["ifconfig", interface, "hw", "ether", new_mac])
-subprocess.call(["ifconfig", interface, "up"])
-subprocess.call(["ifconfig"])
