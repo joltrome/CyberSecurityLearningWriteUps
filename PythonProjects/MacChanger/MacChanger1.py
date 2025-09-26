f5990e7967a94d024ed4a161708c06aad05c19bb
joltrome <93308954+joltrome@users.noreply.github.com>
2025-09-26 17:34:56 +0900

    Update MacChanger1.py

--- PythonProjects/MacChanger/MacChanger1.py
+++ PythonProjects/MacChanger/MacChanger1.py
@@ -2,6 +2,7 @@
 
 import subprocess
 import optparse
+import re
 
 def get_arguments():
     parser = optparse.OptionParser()
@@ -17,16 +18,24 @@ def get_arguments():
 def change_mac(interface,new_mac_address):
     print("[+] Changing MAC Address for " + interface + " to " + new_mac_address)
 
-    # subprocess.call("ifconfig " +  interface + " down", shell=True)
-    # subprocess.call("ifconfig " +  interface + " hw ether " +  new_mac , shell=True)
-    # subprocess.call("ifconfig " +  interface + " up", shell=True)
-    # subprocess.call("ifconfig", shell=True)
-
     subprocess.call(["ifconfig", interface, "down"])
     subprocess.call(["ifconfig", interface, "hw", "ether", new_mac_address])
     subprocess.call(["ifconfig", interface, "up"])
-    subprocess.call(["ifconfig"])
+
+def get_current_mac(interface):
+    ifconfig_interface = subprocess.check_output(["ifconfig", interface])
+    mac_address_search_result = re.search(r"\w\w:\w\w:\w\w:\w\w:\w\w:\w\w", ifconfig_interface)
+    if mac_address_search_result:
+        return mac_address_search_result.group(0)
+    else:
+        print("[-] No MAC Address Found")
 
 options = get_arguments()
+current_mac = get_current_mac(options.interface)
+print("Current MAC Address: " + str(current_mac))
 change_mac(options.interface,options.address)
-
+current_mac = get_current_mac(options.interface)
+if current_mac == options.address:
+    print("[+] MAC Address Changed Successfully to " + options.address)
+else:
+    print("[-] MAC Address did not get changed")
